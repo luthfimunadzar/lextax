860902c2af6418926376d962ce4dc7b6494881ce
Luthfi Munadzar <lmunadzar@yahoo.com>
2020-07-09 21:46:25 +0700

    lextax init

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,26 +15,30 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'LexTax',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', integrity: 'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr', crossorigin: 'anonymous' },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@700&family=Noto+Sans+JP:wght@400;500,700&display=swap" },
     ]
   },
   /*
   ** Global CSS
   */
   css: [
+    { src: '~/assets/css/app.scss', },
   ],
   /*
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
+    { src: '~/plugins/vue-scrollto', ssr: false },
   ],
   /*
   ** Auto import components
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,122 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        lextax
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <header>
+      <h1 class="logo">LEXTAX</h1>
+      <b-link href="#" class="apply"><span><i class="fa fa-pen"></i></span> Apply</b-link>
+    </header>
+
+    <div class="hero">
+      <b-container>
+        <b-row class="d-flex align-items-center">
+          <b-col md="7">
+            <h2>We help businesses taxes adapt at speed</h2>
+            <p>A constantly evolving list of 100 company in indonesia, including an amnesty, monthly tax, and all other tax services.</p>
+            <b-link class="get-started" href="#" v-scroll-to="'#feature'">Get Started <i class="fa fa-long-arrow-alt-down"></i></b-link>
+          </b-col>
+          <b-col md="5">
+            <div class="v-wrap">
+              <video autoplay="autoplay" loop="loop" muted="muted"><source src="/vid2.mp4" type="video/mp4"></video>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
+
+    <div class="feature">
+      <b-container>
+        <b-row>
+          <b-col md="12">
+            <h3 class="title">Our Feature</h3>
+          </b-col>
+          <b-col md="6">
+            <div class="feature-item">
+              <span>01.</span>
+              <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+            </div>
+            <div class="feature-item">
+              <span>02.</span>
+              <p>Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+            </div>
+          </b-col>
+          <b-col md="6">
+            <div class="feature-item">
+              <span>03.</span>
+              <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X.</p>
+            </div>
+            <div class="feature-item">
+              <span>04.</span>
+              <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps.</p>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+
+    <div class="services">
+      <b-container>
+        <b-row>
+          <b-col md="12">
+            <h3 class="title">Our Services</h3>
+          </b-col>
+          <b-col md="8">
+            <div class="service-item" style="background-image: url('https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')">
+              <div class="wrap-text">
+                <h3>Tax Amnesty</h3>
+                <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+              </div>
+            </div>
+          </b-col>
+          <b-col md="4">
+            <div class="service-item" style="background-image: url('https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')">
+              <div class="wrap-text">
+                <h3>Tax Amnesty</h3>
+                <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+              </div>
+            </div>
+          </b-col>
+          <b-col md="4">
+            <div class="service-item" style="background-image: url('https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')">
+              <div class="wrap-text">
+                <h3>Tax Amnesty</h3>
+                <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+              </div>
+            </div>
+          </b-col>
+          <b-col md="8">
+            <div class="service-item" style="background-image: url('https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')">
+              <div class="wrap-text">
+                <h3>Tax Amnesty</h3>
+                <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+              </div>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+
+    <footer>
+      <b-container>
+        <b-row>
+          <b-col md="6">
+            <h2 class="logo">LEXTAX</h2>
+            <p>Jalan Wahid Hasyim No. 10D, Kel. Kebon Sirih, Kec. Menteng, Jakarta Pusat, 10340</p>
+          </b-col>
+          <b-col md="6">
+            <h4>Get in touch</h4>
+            <b-link href="#" class="socmed"><i class="fab fa-facebook-f"></i></b-link>
+            <b-link href="#" class="socmed"><i class="fab fa-twitter"></i></b-link>
+            <b-link href="#" class="socmed"><i class="fab fa-instagram"></i></b-link>
+            <b-link href="#" class="socmed"><i class="fab fa-linkedin-in"></i></b-link>
+          </b-col>
+          <b-col md="6">
+            <p class="mb-0">© 2019 DOMISILI.ID, ALL RIGHTS RESERVED.</p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
